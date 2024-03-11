a298939ce99794bdd48f380e87fc797f7106370e
moritzgladigau <136191908+moritzgladigau@users.noreply.github.com>
2024-03-12 00:37:21 +0100

    homebrew

--- minesweeper_executable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Definiere den Befehl zum Ausführen des Spiels
-alias Minesweeper='/Users/moritzgladigau/Documents/GitHub/Minesweeper/Minesweeper'
-
-# Füge den Pfad zum Spielverzeichnis hinzu
-export PATH=$PATH:/Users/moritzgladigau/Documents/GitHub/Minesweeper/
-
