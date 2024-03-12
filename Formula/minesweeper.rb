2073f629ef6de599ee91dd94e63cf0e54479f85d
moritzgladigau <136191908+moritzgladigau@users.noreply.github.com>
2024-03-12 13:58:45 +0100

    Update minesweeper.rb

--- Formula/minesweeper.rb
+++ Formula/minesweeper.rb
@@ -4,6 +4,8 @@ class Minesweeper < Formula
   url "https://github.com/moritzgladigau/Minesweeper/archive/refs/tags/v1.1.1-beta.tar.gz"
   sha256 "8e7f18d95bf40528c48afd7500075fdbc3816224eac2158e4fa877e590f73d83"
 
+  depends_on "gcc" # Abhängigkeit von GCC
+  
   def install
     # Create the bin directory if it doesn't exist
     bin.mkpath
