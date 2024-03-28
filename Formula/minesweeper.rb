4b12644f9b53a6295ea7ac15394154eb3ced00e1
moritzgladigau <136191908+moritzgladigau@users.noreply.github.com>
2024-03-28 17:52:00 +0100

    n

--- Formula/minesweeper.rb
+++ Formula/minesweeper.rb
@@ -2,7 +2,7 @@ class Minesweeper < Formula
   desc "A Minesweeper game in C for the terminal"
   homepage "https://github.com/moritzgladigau/Minesweeper"
   url "https://github.com/moritzgladigau/Minesweeper/archive/refs/tags/v1.1.1-beta.tar.gz"
-  sha256 "be19409eec54a1d152aaab8eaa38b6894d0b015779e92bc915d6c0e70239e214"
+  sha256 "815c1d2df5cb9844eb0359c91b2c41209ba9b8e4ca77bdca15606c17072f9177"
   # sha256 :no_check
 
   depends_on "gcc" # Abhängigkeit von GCC
