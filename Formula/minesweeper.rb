54987933592ae3cd966720737c17e1925032cc82
moritzgladigau <moritz.gladigau@gmail.com>
2024-04-12 14:16:13 +0200

    Update minesweeper.rb

--- Formula/minesweeper.rb
+++ Formula/minesweeper.rb
@@ -2,7 +2,7 @@ class Minesweeper < Formula
   desc "A Minesweeper game in C for the terminal"
   homepage "https://github.com/moritzgladigau/Minesweeper"
   url "https://github.com/moritzgladigau/Minesweeper/archive/refs/tags/v1.1.1-beta.tar.gz"
-  sha256 "187c3f0adbb028cc86f713e81fd2689160fa11e78a53f2861cb932bb4b0b13e6"
+  sha256 "dc44a156714946f4233cca75eb44e6eb5bdba72549c28ae5277007d1e647af30"
   # sha256 :no_check
 
   depends_on "gcc" # Abhängigkeit von GCC
@@ -24,6 +24,7 @@ class Minesweeper < Formula
   end
 
   def uninstall
+    opoo "Minesweeper wurde deinstalliert."
     # Entfernen Sie das ausführbare Spiel
     rm bin/"minesweeper"
 
