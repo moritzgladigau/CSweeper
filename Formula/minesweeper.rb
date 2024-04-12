9942d5a2fbeefb2c460ca19ae485385e632c4cfb
moritzgladigau <moritz.gladigau@gmail.com>
2024-04-12 12:29:00 +0200

    Update minesweeper.rb

--- Formula/minesweeper.rb
+++ Formula/minesweeper.rb
@@ -4,7 +4,7 @@ class Minesweeper < Formula
   desc "A Minesweeper game in C for the terminal"
   homepage "https://github.com/moritzgladigau/Minesweeper"
   url "https://github.com/moritzgladigau/Minesweeper/archive/refs/tags/v1.1.1-beta.tar.gz"
-  sha256 "680a9b156db831d624b38dca8d81c4a81fb1fab64008c2b3d451267106e8e89b"
+  sha256 "e410c9a83a80748af67b9a607a0bbede4f3e96826ec8a26423ed50a2ad21d8c1"
   # sha256 :no_check
 
   depends_on "gcc" # Abhängigkeit von GCC
@@ -13,6 +13,7 @@ class Minesweeper < Formula
     # Create the bin directory if it doesn't exist
     bin.mkpath
     puts "Creating directory ~/.game"
+    puts "Home directory: #{ENV["HOME"]}"
     mkdir "#{ENV["HOME"]}/.game"
 
     # Build the executable
